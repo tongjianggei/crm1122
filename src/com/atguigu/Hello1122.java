c1588a53c2f1c4a0088d99a2f226649ca16dc6ff
tongjianggei <tongjiangfei@126.com>
2016-12-05 19:56:47 +0800

    谢谢

--- src/com/atguigu/Hello1122.java
+++ src/com/atguigu/Hello1122.java
@@ -5,6 +5,7 @@ public class Hello1122 {
 	public static void main(String[] args) {
 		System.out.println("github hello 01,第一次request");
 		System.out.println("github hello 02,第二次request");
+		System.out.println("收到第一次request");
 	}
 
 }
